3e968349c962e9f0b4e139667ab5acf0ec478052
neil chen <zooming.in@hotmail.com>
2022-05-27 16:26:07 +1000

    fixed database problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'bookmarks#index'
   resources :lists, only: %i[index new create show]
   resources :movies, only: %i[index new create show]
-  resources :bookmarks, only: %i[index new create show edit update destroy]
+  resources :bookmarks, except: [:index]
 end
